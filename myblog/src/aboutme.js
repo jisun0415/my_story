0cc22175f212a4d1bd856cee2e95e1c758471b24
Oh JiSun <ohjeesun@naver.com>
2023-08-14 23:53:02 +0900

    Update introduce

--- myblog/src/aboutme.js
+++ myblog/src/aboutme.js
@@ -1,7 +1,7 @@
 import {Link} from 'react-scroll';
 import './aboutme.css';
 import React from 'react';
-import photo from './myphoto.jpg';
+import photo from './image/myphoto.jpg';
 
 const Aboutme= () =>
 {
@@ -21,20 +21,46 @@ const Aboutme= () =>
                     <img className="jisun" src={photo} alt=""></img>
                 </div>
                 <div className="person">
+                <div className="sentence">
+                목표를 이루기 위해 가장 최적의 수단을 찾는 개미 <br />
+                </div>
                 <p className="name">
                 오 &nbsp;&nbsp; 지 &nbsp;&nbsp; 선<br></br>
                 JISEON OH<br></br>
                 </p>
-                <p className="Contact">
-                #Creative #Executive #Tenacity <br />
-                jisunoh0415@gmail.com<br></br>
-                </p>
+             
+           
                 <p >
+                <a className="Channel"  target="_blank">Notion</a> <br />
                 <a className="Channel" href="https://github.com/jisun0415" target="_blank">Github</a>
-                <br></br>
+                <br /><br /><br />
                 </p>
                 </div>
+            </div>   
+            <div className="intro">
+                    <div className="title">
+                        Introduce
+                    </div>
+                    <div className="Content">
+                    <p className="smalltitle">
+                        E-MAIL <br />
+                        UNIV <br />
+                        BIRTH <br />
+                        POSITION <br />
+                        HOBBY <br />
+                        MY KEYWORDS <br />
+                    </p>
+                    <p className="content">
+                        jisunoh0415@gmail.com <br />
+                        Sejong University <br />
+                        01.04.15 <br />
+                        AI Engineer & Web-Front developer <br />
+                        Running <br />
+                        #Creative #Executive #Tenacity <br />
+                    </p>
+                    </div>
             </div>
+           
         </div>
         <div className="tool" id="scroll-to-tool">
             
